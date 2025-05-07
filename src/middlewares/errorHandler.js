71754ab6fee23f8e7f107e0effc85e247d34bfb3
Souvik <souvikmitra1234@gmail.com>
2025-05-07 20:50:16 +0530

    Remove sample files

--- src/middlewares/errorHandler.js
+++
@@ -1,7 +0,0 @@
-module.exports = (err, req, res, next) => {
-    const status = err.statusCode || 500;
-    const message = err.message || 'Internal Server Error';
-    res
-        .status(status)
-        .render('error', { message });
-};
--- src/routes/api.route.js
+++
@@ -1,8 +0,0 @@
-const path = require("path");
-const express = require("express");
-const router = express.Router();
-const apiController = require(path.join(__dirname, "../controllers/api.controller.js"));
-
-router.get("/create", apiController.homepage);
-
-module.exports = router;
